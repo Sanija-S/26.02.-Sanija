79496725b7ad28d4c517bbf09ec0ba23580f2ca3
Sanija <sanijasokirka@gmail.com>
2024-02-26 08:41:37 +0200

    11414

---
+++ 1uzdevums.py
@@ -0,0 +1,25 @@
+import PySimpleGUI as sg
+dalas=[
+    
+    [sg.Text("Izvēlies attēlu:"), sg.InputText(key="file_path"), sg.FileBrowse()],
+    [sg.Image(key="image")],# Image- klase
+    [sg.Button("Pārādīt attēlu"), sg.Button("Iziet")]
+]
+
+
+logs=sg.Window("Attēlu bibliotēka", dalas,resizable=True)# resizable- logs pielagosies ta elementu izmeriem
+
+
+while True:
+    event, values=logs.read()
+
+    if event==sg.WIN_CLOSED or event=="Iziet": 
+        break
+    if event== "Pārādīt attēlu":
+       file_path=values("file_path")
+       if file_path:
+           
+
+
+
+logs.close()
